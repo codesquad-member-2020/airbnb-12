d72fe3e8da7cecd412837459cbfd7e3d562dc36a
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-05-18 17:41:56 +0900

    chore :sparkles: 클래스 별 폴더 분리 및 프로젝트 세팅
    BE, FE 폴더 분리
    프로젝트 세팅

---
+++ BE/src/main/java/kr/codesquad/airbnb12/Airbnb12Application.java
@@ -0,0 +1,13 @@
+package kr.codesquad.airbnb12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Airbnb12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Airbnb12Application.class, args);
+    }
+
+}
---
+++ BE/src/test/java/kr/codesquad/airbnb12/Airbnb12ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.codesquad.airbnb12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Airbnb12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
