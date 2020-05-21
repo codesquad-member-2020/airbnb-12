5201a5c3d463923fe1dbc5a3ef24fef4d765370e
ragdoll <museopkim0214@gmail.com>
2020-05-21 17:12:53 +0900

    Revert "fix :pushpin: 스키마 수정"
    This reverts commit c9a066cf10899b425f1518cf2ca61bef8483d984.

--- BE/src/main/resources/schema.sql
+++ BE/src/main/resources/schema.sql
@@ -1,3 +1,9 @@
+# DROP TABLE IF EXISTS image;
+# DROP TABLE IF EXISTS booking;
+# DROP TABLE IF EXISTS accommodation;
+# DROP TABLE IF EXISTS location;
+# DROP TABLE IF EXISTS user;
+
 CREATE TABLE IF NOT EXISTS user (
     id    BIGINT AUTO_INCREMENT,
     email VARCHAR(64),
@@ -29,19 +35,18 @@ CREATE TABLE IF NOT EXISTS accommodation (
 );
 
 CREATE TABLE IF NOT EXISTS booking (
-    id               BIGINT,
-    adult            TINYINT,
-    child            TINYINT,
-    infant           TINYINT,
-    nights           SMALLINT,
-    final_price      FLOAT(3, 2),
+    id            BIGINT,
+    adult         TINYINT,
+    child         TINYINT,
+    infant        TINYINT,
+    nights        SMALLINT,
+    final_price   FLOAT(3, 2),
     check_in_date    DATE,
-    check_out_date   DATE,
-    bookable         TINYINT(1),
-    service_fee      FLOAT(3, 2),
-    tourism_tax      FLOAT(3, 2),
-    user             BIGINT,
-    accommodation    BIGINT,
+    check_out_date      DATE,
+    bookable      TINYINT(1),
+    tourism_tax   FLOAT(3, 2),
+    user          BIGINT,
+    accommodation BIGINT,
     PRIMARY KEY (id),
     FOREIGN KEY (user) REFERENCES user (id),
     FOREIGN KEY (accommodation) REFERENCES accommodation (id)
