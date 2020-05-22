a24b22ecf1b5525cf8b30f8ef77cb6f71c30f9e5
ragdoll <museopkim0214@gmail.com>
2020-05-22 16:19:37 +0900

    fix :pushpin: FilteredAccommodationsResponseDto 수정
    #14
    - 기존 도메인과 VO를 혼용하던 방식에서 별도로 분리하도록 수정

--- BE/src/main/java/kr/codesquad/airbnb12/dto/FilteredAccommodationsResponseDto.java
+++ BE/src/main/java/kr/codesquad/airbnb12/dto/FilteredAccommodationsResponseDto.java
@@ -1,7 +1,5 @@
 package kr.codesquad.airbnb12.dto;
 
-import kr.codesquad.airbnb12.domain.Accommodation;
-
 import java.util.List;
 
 public class FilteredAccommodationsResponseDto {
@@ -10,6 +8,41 @@ public class FilteredAccommodationsResponseDto {
 
     private List<Integer> priceDistribution;
 
-    private List<Accommodation> accommodations;
+    private List<AccommodationSummary> accommodations;
 
+    private FilteredAccommodationsResponseDto(int totalCount,
+                                             List<Integer> priceDistribution,
+                                             List<AccommodationSummary> accommodations) {
+        this.totalCount = totalCount;
+        this.priceDistribution = priceDistribution;
+        this.accommodations = accommodations;
+    }
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(int totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public List<Integer> getPriceDistribution() {
+        return priceDistribution;
+    }
+
+    public void setPriceDistribution(List<Integer> priceDistribution) {
+        this.priceDistribution = priceDistribution;
+    }
+
+    public List<AccommodationSummary> getAccommodations() {
+        return accommodations;
+    }
+
+    public void setAccommodations(List<AccommodationSummary> accommodations) {
+        this.accommodations = accommodations;
+    }
+
+    public static FilteredAccommodationsResponseDto create(int totalCount, List<Integer> priceDistribution, List<AccommodationSummary> accommodations) {
+        return new FilteredAccommodationsResponseDto(totalCount, priceDistribution, accommodations);
+    }
 }
