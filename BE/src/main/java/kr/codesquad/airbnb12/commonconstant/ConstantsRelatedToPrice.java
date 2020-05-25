f882a86c6b9e7da69692f8d08b78469af851f2d1
ragdoll <museopkim0214@gmail.com>
2020-05-25 13:24:25 +0900

    fix :pushpin: 가격 분포도 조회 단위 변경
    #14
    $20 -> $5

--- BE/src/main/java/kr/codesquad/airbnb12/commonconstant/ConstantsRelatedToPrice.java
+++ BE/src/main/java/kr/codesquad/airbnb12/commonconstant/ConstantsRelatedToPrice.java
@@ -2,5 +2,5 @@ package kr.codesquad.airbnb12.commonconstant;
 
 public class ConstantsRelatedToPrice {
     public static final int PRICE_DISTRIBUTION_SIZE = 50;
-    public static final int PRICE_RANGE_DIFFERENCE = 20;
+    public static final int PRICE_RANGE_DIFFERENCE = 5;
 }
