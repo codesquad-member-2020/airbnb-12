11b9647f5539f4eda93e658ef337edfa9e8d6b52
ragdoll <museopkim0214@gmail.com>
2020-06-01 15:51:00 +0900

    feat :rocket: 숙소 예약 관련 정보 DTO 생성
    #21
    - 필드 값 수정 : salePrice -> finalPrice
    - 빌더 생성

--- BE/src/main/java/kr/codesquad/airbnb12/dto/BookingDetailsResponseDto.java
+++ BE/src/main/java/kr/codesquad/airbnb12/dto/BookingDetailsResponseDto.java
@@ -2,9 +2,11 @@ package kr.codesquad.airbnb12.dto;
 
 public class BookingDetailsResponseDto {
 
+    private Long accommodationId;
+
     private double originalPrice;
 
-    private double salePrice;
+    private double finalPrice;
 
     private double cleaningFee;
 
@@ -12,4 +14,116 @@ public class BookingDetailsResponseDto {
 
     private double tourismTax;
 
+    public BookingDetailsResponseDto() { }
+
+    public BookingDetailsResponseDto(Long accommodationId, double originalPrice, double finalPrice, double cleaningFee, double serviceFee, double tourismTax) {
+        this.accommodationId = accommodationId;
+        this.originalPrice = originalPrice;
+        this.finalPrice = finalPrice;
+        this.cleaningFee = cleaningFee;
+        this.serviceFee = serviceFee;
+        this.tourismTax = tourismTax;
+    }
+
+    public Long getAccommodationId() {
+        return accommodationId;
+    }
+
+    public void setAccommodationId(Long accommodationId) {
+        this.accommodationId = accommodationId;
+    }
+
+    public double getOriginalPrice() {
+        return originalPrice;
+    }
+
+    public void setOriginalPrice(double originalPrice) {
+        this.originalPrice = originalPrice;
+    }
+
+    public double getFinalPrice() {
+        return finalPrice;
+    }
+
+    public void setFinalPrice(double finalPrice) {
+        this.finalPrice = finalPrice;
+    }
+
+    public double getCleaningFee() {
+        return cleaningFee;
+    }
+
+    public void setCleaningFee(double cleaningFee) {
+        this.cleaningFee = cleaningFee;
+    }
+
+    public double getServiceFee() {
+        return serviceFee;
+    }
+
+    public void setServiceFee(double serviceFee) {
+        this.serviceFee = serviceFee;
+    }
+
+    public double getTourismTax() {
+        return tourismTax;
+    }
+
+    public void setTourismTax(double tourismTax) {
+        this.tourismTax = tourismTax;
+    }
+
+    public static class Builder {
+        Long accommodationId;
+        double originalPrice;
+        double finalPrice;
+        double cleaningFee;
+        double serviceFee;
+        double tourismTax;
+
+        public Builder() { }
+
+        public Builder(BookingDetailsResponseDto bookingDetailsResponseDto) {
+            this.accommodationId = bookingDetailsResponseDto.accommodationId;
+            this.originalPrice = bookingDetailsResponseDto.originalPrice;
+            this.finalPrice = bookingDetailsResponseDto.finalPrice;
+            this.cleaningFee = bookingDetailsResponseDto.cleaningFee;
+            this.serviceFee = bookingDetailsResponseDto.serviceFee;
+            this.tourismTax = bookingDetailsResponseDto.tourismTax;
+        }
+
+        public Builder accommodationId(Long accommodationId) {
+            this.accommodationId = accommodationId;
+            return this;
+        }
+
+        public Builder originalPrice(double originalPrice) {
+            this.originalPrice = originalPrice;
+            return this;
+        }
+
+        public Builder finalPrice(double finalPrice) {
+            this.finalPrice = finalPrice;
+            return this;
+        }
+
+        public Builder cleaningFee(double cleaningFee) {
+            this.cleaningFee = cleaningFee;
+            return this;
+        }
+
+        public Builder serviceFee(double serviceFee) {
+            this.serviceFee = serviceFee;
+            return this;
+        }
+
+        public Builder tourismTax(double tourismTax) {
+            this.tourismTax = tourismTax;
+            return this;
+        }
+
+        public BookingDetailsResponseDto build() {
+            return new BookingDetailsResponseDto(accommodationId, originalPrice, finalPrice, cleaningFee, serviceFee, tourismTax);
+        }
+    }
 }
