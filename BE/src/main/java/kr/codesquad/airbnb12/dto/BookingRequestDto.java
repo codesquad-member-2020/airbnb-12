515b46b37d45ed5fb8cd3969b98fa8abf0b3f316
ragdoll <museopkim0214@gmail.com>
2020-06-02 13:41:06 +0900

    feat :rocket: 예약 요청 Dto 추가
    #21
    - BookingRequestDto
    - adults, children, infants
    - checkInDate, checkOutDate

--- BE/src/main/java/kr/codesquad/airbnb12/dto/BookingRequestDto.java
+++ BE/src/main/java/kr/codesquad/airbnb12/dto/BookingRequestDto.java
@@ -4,8 +4,6 @@ import java.time.LocalDate;
 
 public class BookingRequestDto {
 
-    private Long accommodationId;
-
     private int adults;
 
     private int children;
@@ -15,4 +13,54 @@ public class BookingRequestDto {
     private LocalDate checkInDate;
 
     private LocalDate checkOutDate;
+
+    public BookingRequestDto() { }
+
+    public BookingRequestDto(int adults, int children, int infants, LocalDate checkInDate, LocalDate checkOutDate) {
+        this.adults = adults;
+        this.children = children;
+        this.infants = infants;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+    }
+
+    public int getAdults() {
+        return adults;
+    }
+
+    public void setAdults(int adults) {
+        this.adults = adults;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public int getInfants() {
+        return infants;
+    }
+
+    public void setInfants(int infants) {
+        this.infants = infants;
+    }
+
+    public LocalDate getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(LocalDate checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public LocalDate getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(LocalDate checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
 }
