1283a92015296dc49c47a3b5d2be238bef8f7316
baekCode <sosaer88@gmail.com>
2020-06-04 16:49:32 +0900

    feat: 인원 폼 스타일링
    issue #28

--- client/src/components/SearchForm/FormGuest.jsx
+++ client/src/components/SearchForm/FormGuest.jsx
@@ -4,6 +4,78 @@ import { reset, save } from '../../modules/guest';
 import { GUEST } from '../../constants';
 import styled from 'styled-components';
 
+const FormGuestWrap = styled.div`
+  padding: 0 20%;
+`;
+const GuestCounterWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const GeustCounterDiv = styled.div`
+  padding: 20px 40px;
+  border-left: 1px solid rgba(0, 0, 0, 0.05);
+  p {
+    font-size: 16px;
+    span {
+      display: block;
+      margin: 5px 0 10px;
+      font-size: 14px;
+      color: #888;
+    }
+  }
+  div > span {
+    display: inline-block;
+    width: 50px;
+    text-align: center;
+  }
+  button {
+    width: 30px;
+    height: 30px;
+    font-size: 16px;
+    color: #717171;
+    cursor: pointer;
+    outline: none;
+    border: 1px solid #b7b7b7;
+    border-radius: 20px;
+    background: #fff;
+    &:hover {
+      color: #333;
+      border: 1px solid #333;
+    }
+    &:disabled {
+      cursor: default;
+      color: #ebebeb;
+      border: 1px solid #ebebeb;
+      &:hover {
+        color: #ebebeb;
+        border: 1px solid #ebebeb;
+      }
+    }
+  }
+  &:first-child {
+    border-left: 0;
+  }
+`;
+
+const GuestCounter = ({ title, subtitle, count, increase, decrease }) => {
+  return (
+    <GeustCounterDiv>
+      <p>
+        {title}
+        <span>{subtitle}</span>
+      </p>
+      <div>
+        <button onClick={decrease} disabled={count === 0 && true}>
+          -
+        </button>
+        <span>{count}</span>
+        <button onClick={increase}>+</button>
+      </div>
+    </GeustCounterDiv>
+  );
+};
+
 const FormGuest = () => {
   const dispatch = useDispatch();
   const { adults, children, infants } = useSelector(state => ({
@@ -30,40 +102,33 @@ const FormGuest = () => {
   const onClickIncreaseInfants = () => setCount({ ...count, infants: count.infants + 1 });
 
   return (
-    <div>
-      <p>
-        {GUEST.ADULTS.TITLE}
-        <span>{GUEST.ADULTS.SUBTITLE}</span>
-      </p>
-      <div>
-        <button onClick={onClickDecreaseAdults}>-</button>
-        <span>{count.adults}</span>
-        <button onClick={onClickIncreaseAdults}>+</button>
-      </div>
-
-      <p>
-        {GUEST.CHILDREN.TITLE}
-        <span>{GUEST.CHILDREN.SUBTITLE}</span>
-      </p>
-      <div>
-        <button onClick={onClickDecreaseChildren}>-</button>
-        <span>{count.children}</span>
-        <button onClick={onClickIncreaseChildren}>+</button>
-      </div>
-
-      <p>
-        {GUEST.INFANTS.TITLE}
-        <span>{GUEST.INFANTS.SUBTITLE}</span>
-      </p>
-      <div>
-        <button onClick={onClickDecreaseInfants}>-</button>
-        <span>{count.infants}</span>
-        <button onClick={onClickIncreaseInfants}>+</button>
-      </div>
-
+    <FormGuestWrap>
+      <GuestCounterWrap>
+        <GuestCounter
+          title={GUEST.ADULTS.TITLE}
+          subtitle={GUEST.ADULTS.SUBTITLE}
+          count={count.adults}
+          increase={onClickIncreaseAdults}
+          decrease={onClickDecreaseAdults}
+        />
+        <GuestCounter
+          title={GUEST.CHILDREN.TITLE}
+          subtitle={GUEST.CHILDREN.SUBTITLE}
+          count={count.children}
+          increase={onClickIncreaseChildren}
+          decrease={onClickDecreaseChildren}
+        />
+        <GuestCounter
+          title={GUEST.INFANTS.TITLE}
+          subtitle={GUEST.INFANTS.SUBTITLE}
+          count={count.infants}
+          increase={onClickIncreaseInfants}
+          decrease={onClickDecreaseInfants}
+        />
+      </GuestCounterWrap>
       <button onClick={onReset}>초기화</button>
       <button onClick={onSave}>저장</button>
-    </div>
+    </FormGuestWrap>
   );
 };
 
