ac4a5f15e48b90fd831e9be28f8b210303b7446e
baekCode <sosaer88@gmail.com>
2020-06-05 00:23:14 +0900

    style: 슬라이더 라벨 스타일 수정

--- client/src/components/SearchForm/AirbnbSlider.jsx
+++ client/src/components/SearchForm/AirbnbSlider.jsx
@@ -2,51 +2,51 @@ import Slider from '@material-ui/core/Slider';
 import { withStyles } from '@material-ui/core/styles';
 
 export function AirbnbThumbComponent(props) {
-    return (
-        <span {...props}>
-            <span className="bar" />
-            <span className="bar" />
-            <span className="bar" />
-        </span>
-    );
+  return (
+    <span {...props}>
+      <span className="bar" />
+      <span className="bar" />
+      <span className="bar" />
+    </span>
+  );
 }
 
 export const AirbnbSlider = withStyles({
-    root: {
-        color: '#000',
-        height: 3,
-        padding: '13px 0',
+  root: {
+    color: '#000',
+    height: 3,
+    padding: '13px 0',
+  },
+  thumb: {
+    height: 27,
+    width: 27,
+    backgroundColor: '#fff',
+    border: '1px solid currentColor',
+    marginTop: -12,
+    marginLeft: -13,
+    boxShadow: '#ebebeb 0 2px 2px',
+    '&:focus, &:hover, &$active': {
+      boxShadow: '#ccc 0 2px 3px 1px',
     },
-    thumb: {
-        height: 27,
-        width: 27,
-        backgroundColor: '#fff',
-        border: '1px solid currentColor',
-        marginTop: -12,
-        marginLeft: -13,
-        boxShadow: '#ebebeb 0 2px 2px',
-        '&:focus, &:hover, &$active': {
-            boxShadow: '#ccc 0 2px 3px 1px',
-        },
-        '& .bar': {
-            height: 9,
-            width: 1,
-            backgroundColor: 'currentColor',
-            marginLeft: 1,
-            marginRight: 1,
-        },
+    '& .bar': {
+      height: 9,
+      width: 1,
+      backgroundColor: 'currentColor',
+      marginLeft: 1,
+      marginRight: 1,
     },
-    active: {},
-    valueLabel: {
-        left: 'calc(-50% + 12px)',
-        top: -22,
-    },
-    track: {
-        height: 3,
-    },
-    rail: {
-        color: '#d8d8d8',
-        opacity: 1,
-        height: 3,
-    },
+  },
+  active: {},
+  valueLabel: {
+    left: 'calc(-50% + 9px)',
+    top: -32,
+  },
+  track: {
+    height: 3,
+  },
+  rail: {
+    color: '#d8d8d8',
+    opacity: 1,
+    height: 3,
+  },
 })(Slider);
